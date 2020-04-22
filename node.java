17d94d9708ed81180e93763375a11d2c465a16fa
Yash Lekhwani <yashlekhwani@gmail.com>
2020-04-22 14:09:00 -0500

    added arrayblockingqueue for handling neighbours

--- node.java
+++ node.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.util.*;
 import java.net.*;
 
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class node
 {
     public node(){}
@@ -11,25 +13,56 @@ public class node
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter port number for your node! (Any number between 3000 and 8000)");
         int nodePort = scan.nextInt();
+        System.out.println("Eneter port number of node you want to connect to!");
+        int network = scan.nextInt();
         scan.close();
-        (new Listener(nodePort)).start(); //start listening on node port
+
+        ArrayBlockingQueue<Integer> list = new ArrayBlockingQueue<Integer>(80);
+        list.add(new Integer(network)); //add connection to node
+
+        (new Listener(nodePort, list)).start(); //start listening on node port
 
         try
         {
-            //Connecting to Network
-            String ip = "127.0.0.1";
-            int network = 6580;
-            System.out.println("Not Connected");
-            Socket clientSocket = new Socket(ip, network);
-            System.out.println("Connected");
-            
-            DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
+            ping(list, nodePort);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Exception: " + e);
+        }
+    }
 
-            while(true)
+    public static void ping(ArrayBlockingQueue<Integer> list, int nodePort)
+    {
+        try{
+
+            if (list.size() == 0)
             {
-                String msg = nodePort + ",Ping from Node.";
-                output.write(msg.getBytes()); //send message
-                System.out.println("Pinged Server");
+                Thread.sleep(2000);
+                ping(list, nodePort);
+            }
+
+            while (true)
+            {
+                Integer[] array = list.toArray(new Integer[0]);
+                System.out.println(array[0]);
+                for(int i = 0; i < list.size(); i++)
+                {
+                    //Connecting to Network
+                    String ip = "127.0.0.1";
+                    System.out.println("Not Connected");
+                    Socket clientSocket = new Socket(ip, (int) array[i]);
+                    System.out.println("Connected");
+                    
+                    DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
+
+                    String msg = nodePort + ",Ping from Node.";
+                    output.write(msg.getBytes()); //send message
+                    System.out.println("Pinged Server");
+
+                    clientSocket.close();
+                    output.close();
+                }
                 Thread.sleep(2000);
             }
         }
@@ -42,7 +75,13 @@ public class node
 class Listener extends Thread
 {
     int port = 0;
-    public Listener(int x) { port = x; }
+    ArrayBlockingQueue<Integer> list = new ArrayBlockingQueue<Integer>(80);
+    
+    public Listener(int x, ArrayBlockingQueue<Integer> l) 
+    { 
+        port = x;
+        list = l;
+    }
 
     public void run()
     {
